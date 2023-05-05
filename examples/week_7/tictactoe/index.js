7c2938a77218c03a9664983e21dcdee09451d945
Trevor Austin <trevor@scenery.video>
2023-05-05 18:25:48 -0500

    Update example starter code with new stylesheet

--- examples/week_7/tictactoe/index.js
+++ examples/week_7/tictactoe/index.js
@@ -1,20 +1,22 @@
 function Board() {
   return (
     <div className="game">
-      <div className="board-row">
-        <button className="square">1</button>
-        <button className="square">2</button>
-        <button className="square">3</button>
-      </div>
-      <div className="board-row">
-        <button className="square">4</button>
-        <button className="square">5</button>
-        <button className="square">6</button>
-      </div>
-      <div className="board-row">
-        <button className="square">7</button>
-        <button className="square">8</button>
-        <button className="square">9</button>
+      <div className="game-board">
+        <div className="board-row">
+          <button className="square">1</button>
+          <button className="square">2</button>
+          <button className="square">3</button>
+        </div>
+        <div className="board-row">
+          <button className="square">4</button>
+          <button className="square">5</button>
+          <button className="square">6</button>
+        </div>
+        <div className="board-row">
+          <button className="square">7</button>
+          <button className="square">8</button>
+          <button className="square">9</button>
+        </div>
       </div>
     </div>
   );
