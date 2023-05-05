a3dd03e8ec1f7d5c4dd343b94ae7b01c7a4597d4
Trevor Austin <trevor@scenery.video>
2023-05-05 16:49:29 -0500

    Add week 7 notes and in-class example

---
+++ examples/week_7/declarative/state.js
@@ -0,0 +1,11 @@
+let state = {
+    user: {
+        id: 128,
+        name: "Alice"
+    },
+    activePage: "room",
+    room_id: 27,
+    messages: [...]
+}
+
+renderPage(state);
---
+++ examples/week_7/tictactoe/index.js
@@ -0,0 +1,27 @@
+function Board() {
+  return (
+    <div className="game">
+      <div className="board-row">
+        <button className="square">1</button>
+        <button className="square">2</button>
+        <button className="square">3</button>
+      </div>
+      <div className="board-row">
+        <button className="square">4</button>
+        <button className="square">5</button>
+        <button className="square">6</button>
+      </div>
+      <div className="board-row">
+        <button className="square">7</button>
+        <button className="square">8</button>
+        <button className="square">9</button>
+      </div>
+    </div>
+  );
+}
+
+// ========================================
+
+const rootContainer = document.getElementById("root");
+const root = ReactDOM.createRoot(rootContainer);
+root.render(<Board />);
