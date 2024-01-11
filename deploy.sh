4f06cbb41e3f32a56ff6bf09e6145ec706136831
Trevor Austin <trevoraustin@cs.uchicago.edu>
2024-01-11 17:42:59 -0600

    Clean up file structure

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,2 @@
-rsync -v -r examples student@uchicagowebdev.com:/var/www/html
-rsync -v -r images student@uchicagowebdev.com:/var/www/html/course_lectures
-rsync -v *.md *.css *.html student@uchicagowebdev.com:/var/www/html/course_lectures
-rsync -v uchicagowebdev.com/* trevor@uchicagowebdev.com:/var/www/html/
+rsync -v -r examples images *.md *.css remark.html trevor@uchicagowebdev.com:/var/www/html
+rsync -v index.html trevor.html trevor@uchicagowebdev.com:/var/www/html/
