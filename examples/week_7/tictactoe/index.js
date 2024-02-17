b97fd7652760cabc04e18e6836fa0edc0713a010
Trevor Austin <trevoraustin@cs.uchicago.edu>
2024-02-16 18:09:50 -0600

    Reset tic tac toe

--- examples/week_7/tictactoe/index.js
+++ examples/week_7/tictactoe/index.js
@@ -1,79 +1,16 @@
-function Square({ value, onSquareClick }) {
-  return (
-    <button className="square" onClick={onSquareClick}>
-      {value}
-    </button>
-  );
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles.css";
+
+// import App from "./App";
+
+export default function Square() {
+  return <button className="square">X</button>;
 }
 
-function Board() {
-  const [xIsNext, setXIsNext] = React.useState(true); // [state, setState
-  const [squares, setSquares] = React.useState(Array(9).fill(null));
-
-  function handleClick(i) {
-    if (squares[i] || calculateWinner(squares)) {
-      return;
-    }
-
-    const newSquares = squares.slice();
-
-    newSquares[i] = xIsNext ? "X" : "O"; // squares[i] is unchanged
-    setSquares(newSquares);
-    setXIsNext(!xIsNext);
-  }
-
-  function calculateWinner(squares) {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (
-        squares[a] &&
-        squares[a] === squares[b] &&
-        squares[a] === squares[c]
-      ) {
-        return squares[a];
-      }
-    }
-    return null;
-  }
-
-  const winner = calculateWinner(squares);
-
-  return (
-    <div className="game">
-      <div className="game-board">
-        <div className="board-row">
-          <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-          <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-          <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-        </div>
-        <div className="board-row">
-          <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-          <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-          <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-        </div>
-        <div className="board-row">
-          <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-          <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-          <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-        </div>
-      </div>
-      {winner && <div>{winner} wins the game!</div>}
-    </div>
-  );
-}
-
-// ========================================
-
-const rootContainer = document.getElementById("root");
-const root = ReactDOM.createRoot(rootContainer);
-root.render(<Board />);
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
