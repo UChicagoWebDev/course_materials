e3a589a8abf06fc42a7e220e5c2d1547d7e66dfb
Trevor Austin <trevoraustin@cs.uchicago.edu>
2024-04-28 15:32:59 -0500

    Finish spa example

--- examples/week_6/spa/static/script.js
+++ examples/week_6/spa/static/script.js
@@ -20,14 +20,31 @@ function makeBest(animal) {
     document.title = "SPA: Single Page Application";
   }
     // TODO: push /animal to the URL bar and add this page to the history
+    // "127.0.0.1/dog"
     // DOES NOT WORK: location.replace('/'+animal)
+    history.pushState(null, null, animal);
 }
 
 function loadAnimal() {
   animal = "";
 
+  let path = window.location.pathname;
+  let parsed = path.split("/")[1];
+  if (parsed.length > 1) {
+    animal = parsed;
+  }
+
   // TODO: Check the URL bar on load so e.g. /cat makes cat best
   console.log("Making best animal: " + animal);
-  makeBest(animal);
+  showHappyAnimal(animal);
 }
+
+window.addEventListener("load", ()=>{
+  console.log("load");
+  loadAnimal();
+});
+addEventListener("popstate", ()=>{
+  console.log("popstate"); 
+  loadAnimal();
+});
 
