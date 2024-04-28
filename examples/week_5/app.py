42438e23e90ae8602a8af238c25c3ecf18668a56
Trevor Austin <trevoraustin@cs.uchicago.edu>
2024-04-28 15:27:06 -0500

    Row row rowserver

--- examples/week_5/app.py
+++ examples/week_5/app.py
@@ -1,5 +1,5 @@
 # Row Row Row Your API
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, redirect
 from markupsafe import escape
 
 app = Flask(__name__)
@@ -10,22 +10,17 @@ song =  {"lines": [
     "Merrily merrily merrily merrily",
     "Life is but a dream"]}
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def hello():
-    return song
+    if request.method == "GET":
+        return song
+    if request.method == "POST":
+        return {}, 403
 
-@app.route('/line/<int:line_number>', methods=['GET', 'POST'])
-def get_line(line_number):
-    print(song)
-    if(request.method == "GET"):
-        return song["lines"][line_number]
-    elif(request.method == "POST"):
-        request.get_data()
-        print("we got a POST")
-        print(request.data)
-        new_line = request.data.decode("utf-8")
-        song["lines"][line_number] = new_line
-        return song["lines"][line_number]
-    print("wtf?")
-    # TODO: Add a POST method that add or updates a line to the song
 
+@app.route('/lines/<int:n>')
+def firstLine(n):
+    if n < 1 or n > 4:
+        return redirect("/")
+
+    return song['lines'][n-1]
