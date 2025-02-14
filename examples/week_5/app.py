fc3c0a7f5a63f8358d628708a2ebeb0a29b28494
Trevor Austin <trevoraustin@cs.uchicago.edu>
2025-02-14 16:16:49 -0600

    Fix spacing on row row boat api

--- examples/week_5/app.py
+++ examples/week_5/app.py
@@ -2,11 +2,13 @@
 from flask import Flask, request, jsonify, redirect
 from markupsafe import escape
 app = Flask(__name__)
+
 song =  {"lines": [
     "Row row row your boat",
     "Gently down the stream",
     "Merrily merrily merrily merrily",
     "Life is but a dream"]}
+
 @app.route('/', methods=['GET'])
 def hello():
     if request.method == "GET":
